f1379c81d11933ee9d621c8a1a68d23bd9acc00a
JaiDhiman <jaidhiman2000@gmail.com>
2025-03-27 18:20:07 -0700

    init

--- src/lib/supabase.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import { env } from "$env/dynamic/public";
-
-if (!env.PUBLIC_SUPABASE_URL || !env.PUBLIC_SUPABASE_ANON_KEY) {
-  throw new Error("Missing Supabase environment variables");
-}
-
-export const supabase = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
