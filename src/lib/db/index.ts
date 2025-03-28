2ce5188dbbfdd4de0e886831bb03169f8f519eec
JaiDhiman <jaidhiman2000@gmail.com>
2025-03-28 02:46:47 -0700

    init

--- src/lib/db/index.ts
+++
@@ -1,5 +0,0 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import { supabase } from "../supabase";
-import * as schema from "./schema";
-
-export const db = drizzle(supabase.connectionString as any, { schema });
