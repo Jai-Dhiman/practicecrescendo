e498350e08f133f7b72efeaeb76930c52d951ae0
JaiDhiman <jaidhiman2000@gmail.com>
2025-03-29 02:35:52 -0700

    working ui mockup

--- src/lib/server/index.ts
+++ src/lib/server/index.ts
@@ -1,7 +1,19 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const connectionString = process.env.DATABASE_URL;
 
-const client = postgres(connectionString!, { prepare: false });
+if (!connectionString) {
+  console.error("DATABASE_URL environment variable is not defined");
+  process.exit(1);
+}
+
+const client = postgres(connectionString, {
+  prepare: false,
+  ssl: "require",
+});
+
 export const db = drizzle(client);
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,15 +3,12 @@ const daisyui = require("daisyui");
 /** @type {import('tailwindcss').Config}*/
 const config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
-
   theme: {
     extend: {},
   },
-
   plugins: [daisyui],
-
   daisyui: {
-    themes: ["emerald"],
+    themes: ["silk"],
   },
 };
 
